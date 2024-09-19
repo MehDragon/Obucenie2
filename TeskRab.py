a3ae272d3068c1bba4e3183bdf1dadd6d5cf92d2
MehDragon <95519818+MehDragon@users.noreply.github.com>
2024-09-19 18:57:06 +0300

    Update TeskRab.py
    ffff

--- TeskRab.py
+++ TeskRab.py
@@ -1,5 +1,37 @@
-def main():
-    print("Скрипт успешно запущен.")
+tasks = {}
 
-if __name__ == "__main__":
-    main()
+
+def get_id(tasks_dict: dict) -> int:
+    length_tasks = len(tasks)
+    return length_tasks + 1
+
+
+def add_task(name: str = None):
+    id_ = get_id(tasks_dict=tasks)
+    tasks[id_] = name
+
+def remove_tasks(id_: int):
+    tasks.pop(id_)
+
+
+def sort_tasks():
+    for task in tasks: ...
+
+
+flag = True
+
+while flag:
+    action = input('Введите действие: ')
+    if action == 'add':
+        name = input('Введите название задача: ')
+        add_task(name=tasks_name)
+        print(tasks)
+    if action == 'remove':
+        task_id = int(input('Введите id задачи: '))
+        remove_tasks(task_id)
+    if action == 'show':
+        print(tasks)
+    if action == 'exit':
+        break
+
+print(tasks)
