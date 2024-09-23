b84d72fbc5d069923ff7f06ae7e26102be3bf011
MehDragon <95519818+MehDragon@users.noreply.github.com>
2024-09-23 18:11:23 +0300

    Update TeskRab.py
    DZ

--- TeskRab.py
+++ TeskRab.py
@@ -1,37 +1,12 @@
-tasks = {}
+char = str(input('Введите символ: '))
 
+for i in range (1,6):
+   print(char*i)
 
-def get_id(tasks_dict: dict) -> int:
-    length_tasks = len(tasks)
-    return length_tasks + 1
+char = str(input('Введите символ: '))
 
-
-def add_task(name: str = None):
-    id_ = get_id(tasks_dict=tasks)
-    tasks[id_] = name
-
-def remove_tasks(id_: int):
-    tasks.pop(id_)
-
-
-def sort_tasks():
-    for task in tasks: ...
-
-
-flag = True
-
-while flag:
-    action = input('Введите действие: ')
-    if action == 'add':
-        name = input('Введите название задача: ')
-        add_task(name=tasks_name)
-        print(tasks)
-    if action == 'remove':
-        task_id = int(input('Введите id задачи: '))
-        remove_tasks(task_id)
-    if action == 'show':
-        print(tasks)
-    if action == 'exit':
-        break
-
-print(tasks)
+print (char)
+print (char+char)
+print (char+char+char)
+print (char+char+char+char)
+print (char+char+char+char+char)
