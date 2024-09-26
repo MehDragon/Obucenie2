56dd0e5f1a0f51e536d84fec3f267b7c6d18f17b
MehDragon <95519818+MehDragon@users.noreply.github.com>
2024-09-26 17:18:41 +0300

    dz

--- TeskRab.py
+++ TeskRab.py
@@ -20,3 +20,21 @@ if cleaned_word == cleaned_word[::-1]:
     print("Слово является палиндромом.")
 else:
     print("Слово не является палиндромом.")
+
+
+def sum_pairs(numbers):
+    sums = []
+
+    for i in range(0, len(numbers) - 1, 2):
+        pair_sum = numbers[i] + numbers[i + 1]
+        sums.append(pair_sum)
+
+    return sums
+
+input_numbers = input("Введите числа через пробел: ")
+
+number_list = list(map(int, input_numbers.split()))
+
+result = sum_pairs(number_list)
+print("Суммы пар чисел:", result)
+
