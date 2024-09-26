457620a9b6e1208405a1db651def3ab4612a3c03
MehDragon <95519818+MehDragon@users.noreply.github.com>
2024-09-26 16:57:30 +0300

    DZ
    dd

--- TeskRab.py
+++ TeskRab.py
@@ -1,9 +1,22 @@
 nums = input("Введите числа через пробел: ")
 
-numbers = list(map(int, nums.split()))
+numbers = []
+for num_str in nums.split():
+    numbers.append(int(num_str))
+
 even_numbers = []
 for number in numbers:
     if number % 2 == 0:
         even_numbers.append(number)
 
 print("Список чётных чисел:", even_numbers)
+
+
+word = input('Введите слово: ')
+
+cleaned_word = word.replace(" ", "").lower()
+
+if cleaned_word == cleaned_word[::-1]:
+    print("Слово является палиндромом.")
+else:
+    print("Слово не является палиндромом.")
